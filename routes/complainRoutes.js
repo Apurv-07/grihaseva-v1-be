76f08acc720f803b3c6833d89f86f2c7354e214b
Apurva Prakash <mailapurva007@gmail.com>
2025-11-13 17:22:02 +0530

    added auth security

--- routes/complainRoutes.js
+++ routes/complainRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getAllComplaints, raiseComplaint, updateComplaintStatus } from "../contollers/complainController.js";
+import authProtector from "../middlewares/authProtector.js";
 const complainRouter = Router();
 
-complainRouter.get("/", getAllComplaints);
-complainRouter.put("/resolve", updateComplaintStatus);
+complainRouter.get("/", authProtector, getAllComplaints);
+complainRouter.put("/resolve", authProtector, updateComplaintStatus);
 complainRouter.post("/raise", raiseComplaint);
 
 export default complainRouter;
