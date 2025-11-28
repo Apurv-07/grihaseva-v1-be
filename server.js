cd29f20e7f596a5db3896e2d646ae3ca4ed60c29
Apurva Prakash <mailapurva007@gmail.com>
2025-11-28 20:41:33 +0530

    FIXED CORS ERROR

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://newrepo-grihseva-fe-j8en.vercel.app", "https://www.grihseva.com/"],
+    origin: ["http://localhost:3000","https://newrepo-grihseva-fe-j8en.vercel.app", "https://www.grihseva.com"],
     credentials: true,
   })
 );
